5729658fad29d77664043f4d9286fc6d3b60a428
cjones1047 <cjmj1047@gmail.com>
2022-11-30 13:08:19 -0600

    Move method now correctly changes the direction of the ball's movement on the y-axis to the opposite direction when it touches the top or bottom walls

--- ball.py
+++ ball.py
@@ -8,8 +8,12 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
+        self.y_movement = 10
+        self.x_movement = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        if self.ycor() > 280 or self.ycor() < -270:
+            self.y_movement *= -1
+        new_x = self.xcor() + self.x_movement
+        new_y = self.ycor() + self.y_movement
         self.goto(new_x, new_y)
