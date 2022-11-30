ff51955f890e0ab64ddc187c5daf1552ba7db69a
cjones1047 <cjmj1047@gmail.com>
2022-11-30 11:55:52 -0600

    Turtle tracer method passed argument of 0 so that screen rendering can be manually delayed and updated

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
 left_paddle = Turtle()
 left_paddle.color("white")
@@ -17,16 +18,19 @@ left_paddle.shape("square")
 left_paddle.penup()
 left_paddle.shapesize(stretch_wid=5, stretch_len=1)
 left_paddle.goto(-350, 0)
+screen.update()
 
 
 def go_up():
     new_y = left_paddle.ycor() + 20
     left_paddle.goto(left_paddle.xcor(), new_y)
+    screen.update()
 
 
 def go_down():
     new_y = left_paddle.ycor() - 20
     left_paddle.goto(left_paddle.xcor(), new_y)
+    screen.update()
 
 
 screen.listen()
